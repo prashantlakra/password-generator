7a0aa379e97de944cb16009e7fd571c96f2e2fc0
prashantlakra <prashant.lakra@outlook.com>
2024-08-26 19:01:09 +0530

    add password generator

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let generatePasswordButton = document.getElementById(
+  "generate-password-button"
+);
+
+let passwordElOne = document.getElementById("password-el-one");
+let passwordElTwo = document.getElementById("password-el-two");
+
+function generatePassword() {
+  let password = "";
+  for (i = 0; i < 15; i++) {
+    let randomPassword = Math.floor(Math.random() * characters.length);
+    password += characters[randomPassword];
+  }
+  return password;
+}
+
+generatePasswordButton.addEventListener("click", function () {
+  let password1 = generatePassword();
+  let password2 = generatePassword();
+  passwordElOne.textContent = password1;
+  passwordElTwo.textContent = password2;
+});
